e2d3250afa9c8a67947493fa27bf6dc8b6be0381
LuyiLi <luyili08@163.com>
2024-04-04 15:57:24 +0800

    Alter the state machine for better organization

--- me5413_world/src/state_machine_node.py
+++ me5413_world/src/state_machine_node.py
@@ -3,7 +3,6 @@
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped, Pose
-import geometry_msgs.msg
 import tf2_ros
 import tf2_geometry_msgs
 import tf_conversions
@@ -12,21 +11,37 @@ from std_msgs.msg import Float32
 class State:
     def __init__(self, robot):
         self.robot = robot
+    def init(self):
+        pass
+    def execute(self):
+        pass
+
+class Task1Tracking(State):
+    def init(self, goal_pose):
+        self.robot.pub_goal.publish(goal_pose)
+
+    def execute(self):
+        pass
+
+class Task2State(State):
+    def init(self, goal_pose):
+        self.robot.pub_goal.publish(goal_pose)
+    def execute(self):
+        pass
+
+class Task3Tracking(State):
+    def init(self, goal_pose):
+        self.robot.pub_goal.publish(goal_pose)
 
     def execute(self):
         pass
 
 class IdleState(State):
-    def execute(self):
+    def init(self):
         rospy.loginfo("机器人处于待机状态")
 
-class MoveState(State):
     def execute(self):
-        rospy.loginfo("机器人正在移动")
-
-class StopState(State):
-    def execute(self):
-        rospy.loginfo("机器人停止移动")
+        pass
 
 class Robot:
     def __init__(self):
@@ -44,15 +59,18 @@ class Robot:
         self.world_frame = "world"
         self.pose_world_robot = PoseStamped()
         self.pose_map_robot = PoseStamped()
-        
+
+        # State Machine
         self.idle_state = IdleState(self)
-        self.move_state = MoveState(self)
-        self.stop_state = StopState(self)
+        self.task1_state = Task1Tracking(self)
+        self.task2_state = Task2State(self)
+        self.task3_state = Task3Tracking(self)
         self.current_state = self.idle_state
 
 
-    def set_state(self, state):
+    def set_state(self, state, args=None):
         self.current_state = state
+        state.init(args)
 
     def execute_state(self):
         self.current_state.execute()
@@ -62,6 +80,7 @@ class Robot:
         end = goal_name.find('_')
         self.goal_type = goal_name[1:end]
         # goal_box_id = int(goal_name[end+1])
+        print("Goal Type: ", self.goal_type)
 
         P_world_goal = PoseStamped()
         if self.goal_type == "box":
@@ -89,8 +108,15 @@ class Robot:
         self.pose_map_robot = tf2_geometry_msgs.do_transform_pose(self.pose_world_robot, transform_map_world)
 
         # Publish goal pose in map frame 
-        if self.goal_type != "box":
-            self.pub_goal.publish(P_map_goal)
+        if self.goal_type == "assembly":
+            self.set_state(self.task1_state, P_map_goal)
+        elif self.goal_type == "vehicle":
+            self.set_state(self.task3_state, P_map_goal)
+        elif self.goal_type == "box":
+            self.set_state(self.task2_state)
+        else:
+            self.set_state(self.idle_state)
+            rospy.logwarn("Invalid goal type: %s", self.goal_type)
 
     def robot_odom_callback(self, odom):
         self.world_frame = odom.header.frame_id
@@ -124,13 +150,8 @@ class Robot:
 
 def state_callback(data):
     global robot
-    if data.data == "MOVE":
-        robot.set_state(robot.move_state)
-    elif data.data == "STOP":
-        robot.set_state(robot.stop_state)
-    elif data.data == "IDLE":
+    if data.data == "Complete":
         robot.set_state(robot.idle_state)
-
 
 
 if __name__ == '__main__':
@@ -140,5 +161,5 @@ if __name__ == '__main__':
     rospy.Subscriber("robot_state", String, state_callback)
     rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():
-        # robot.execute_state()
+        robot.execute_state()
         rate.sleep()
