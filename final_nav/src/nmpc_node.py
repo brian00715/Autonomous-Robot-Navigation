ec7cd58adf1f244458f3dced0af5aaaa147a8ec1
SimonKenneth <smkk00715@gmail.com>
2024-04-06 22:35:54 +0800

    added perfect mpc and traj interpolation

---
+++ final_nav/src/nmpc_node.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python3
+
+import math
+import os
+import sys
+
+import ipdb
+import numpy as np
+import rospy
+import scipy.linalg
+import tf.transformations as tft
+import yaml
+from dynamic_reconfigure.server import Server as DynServer
+from geometry_msgs.msg import Pose, PoseArray, PoseStamped, Twist
+from nav_msgs.msg import Odometry, Path
+from nmpc_controller import NMPCC
+from utils import (euclidian_dist_se2, extract_interpolated_path,
+                   ndarray2pose_se2, path2ndarray_se2, quat2yaw)
+
+
+class NMPCNode:
+    def __init__(self, method="mpc"):
+        rospy.init_node("path_tracker_node")
+
+        self.flag = True
+        self.method = method
+
+        self.enable_plan = False
+        config_path = os.path.join(sys.path[0], "../config/nav_params/mpc.yaml")
+        param_dict = yaml.safe_load(open(config_path, "r"))
+        self.arrive_th = param_dict["arrive_th"]
+        self.vel_ref = param_dict["vel_ref"]
+
+        if self.method == "mpc":
+            self.N = param_dict["N"]
+            self.T = param_dict["T"]
+            prob_params = {
+                "control_dim": 2,
+                "state_dim": 3,
+                "max_vel": param_dict["max_vel"],
+                "max_lin_acc": param_dict["max_lin_acc"],
+                "max_ang_acc": param_dict["max_ang_acc"],
+                "max_omega": param_dict["max_omega"],
+                "init_pose": np.array([0, 0, 0]),
+            }
+            self.Q = np.diag(param_dict["Q"])
+            self.R = np.diag(param_dict["R"])
+            self.controller = NMPCC(
+                T=self.T,
+                N=self.N,
+                prob_params=prob_params,
+                Q=self.Q,
+                R=self.R,
+            )
+
+        # LQR parameters
+        if self.method == "lqr":
+            self.Q = np.diag([1, 1, 1])
+            self.R = np.diag([0.1, 0.1])
+            self.dt = 0.1
+
+        self.curr_odom = Odometry()
+        self.local_ref_path = None
+        self.curr_pose = None
+        self.global_path = None
+        self.goal_pose = PoseStamped()
+
+        # ROS related
+        self.sub_robot_odom = rospy.Subscriber("/gazebo/ground_truth/state", Odometry, self.robot_odom_callback)
+        self.pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+        # self.local_path_sub = rospy.Subscriber("/me5413_world/planning/local_path", Path, self.local_path_callback)
+        self.global_path_sub = rospy.Subscriber("/move_base/NavfnROS/plan", Path, self.global_path_callback)
+        self.pred_pose_pub = rospy.Publisher("/final_nav/mpc/pred_pose", PoseArray, queue_size=1)
+        self.local_ref_path_pub = rospy.Publisher("/final_nav/mpc/local_path", Path, queue_size=1)
+        self.goal_pose_sub = rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.goal_pose_callback)
+        # self.dyn_client = DynServer(path_publisherConfig, self.dyn_callback)
+
+    def goal_pose_callback(self, msg: PoseStamped):
+        self.enable_plan = True
+        self.goal_pose = msg
+        rospy.loginfo(f"New goal {(msg.pose.position.x,msg.pose.position.y,quat2yaw(msg.pose.orientation))} received!")
+
+    def global_path_callback(self, msg: Path):
+        self.global_path = msg
+        if self.enable_plan:
+            self.local_ref_path = extract_interpolated_path(msg, self.T, self.N + 1, self.vel_ref)
+
+    def find_nearest_point(self):
+        if self.global_path is None or self.curr_pose is None:
+            return None
+
+        min_dist = float("inf")
+        selected_idx = None
+        robot_x, robot_y = self.curr_pose[0], self.curr_pose[1]
+        robot_heading = self.curr_pose[2]
+
+        for i, pose in enumerate(self.global_path.poses):
+            point_x, point_y = pose.pose.position.x, pose.pose.position.y
+            base2point_yaw = math.atan2(point_y - robot_y, point_x - robot_x)
+            dist = math.sqrt((point_x - robot_x) ** 2 + (point_y - robot_y) ** 2)
+
+            # Calculate the absolute angle difference between robot heading and base2point_yaw
+            angle_diff = abs(robot_heading - base2point_yaw)
+            angle_diff = min(angle_diff, 2 * math.pi - angle_diff)  # Normalize angle to be within [0, π]
+
+            # Threshold for angle difference can be adjusted. Here it's set to π/4 radians (45 degrees)
+            if dist < min_dist and angle_diff <= math.pi / 4:
+                min_dist = dist
+                selected_idx = i
+
+        return selected_idx
+
+    def output_tum_format(self, odom, filename="trajectory.txt"):
+        """
+        Output the trajectory tracking data in TUM format given an Odometry message.
+        """
+        # Extract the timestamp
+        timestamp = odom.header.stamp.to_sec()
+
+        # Extract position data
+        position = odom.pose.pose.position
+        x, y, z = position.x, position.y, position.z
+
+        # Extract orientation data in TUM format
+        qx, qy, qz, qw = (
+            odom.pose.pose.orientation.x,
+            odom.pose.pose.orientation.y,
+            odom.pose.pose.orientation.z,
+            odom.pose.pose.orientation.w,
+        )
+
+        # Format the data in TUM format
+        tum_data = f"{timestamp} {x:.4f} {y:.4f} {z:.4f} {qx:.4f} {qy:.4f} {qz:.4f} {qw:.4f}\r\n"
+
+        # For this example, we print the data, but you can choose to write it to a file or handle it differently
+        print(tum_data)
+        filename = "/home/simon/ME5413_Planning_Project/trajectory.txt"
+        with open(filename, "a") as file:
+            file.write(tum_data)
+
+    def dyn_callback(self, config, level):
+        self.vel_ref = config["speed_target"]
+        rospy.loginfo("Reconfigure Request: speed_target={}".format(self.vel_ref))
+        return config
+
+    def pos_to_state(self, pos):
+        yaw = quat2yaw(pos.orientation)
+        state = np.array([pos.position.x, pos.position.y, yaw])
+        return state
+
+    def robot_odom_callback(self, msg: Odometry):
+        if self.flag:
+            self.flag = False
+            pose = msg.pose.pose
+            self.init_pose = self.pos_to_state(pose)
+            return
+        self.world_frame = msg.header.frame_id
+        self.robot_frame = msg.child_frame_id
+        self.curr_odom = msg
+        self.curr_pose = [
+            self.curr_odom.pose.pose.position.x,
+            self.curr_odom.pose.pose.position.y,
+            quat2yaw(self.curr_odom.pose.pose.orientation),
+        ]
+
+        # self.output_tum_format(odom)
+
+    def local_path_callback(self, msg: Path):
+        # self.pose_world_goal_ = path.pose[11].pose
+        # self.pose_world_goal_ 应该包含后续一段时间内的路径规划信息
+        # goal_poses = [pose.pose.position for pose in path.poses]
+        self.local_ref_path = [
+            [
+                msg.poses[i].pose.position.x,
+                msg.poses[i].pose.position.y,
+                quat2yaw(msg.poses[i].pose.orientation),
+            ]
+            for i in range(11, min(len(msg.poses), 11 + self.N + 1))
+        ]
+        # if len(self.local_ref_path) < self.N + 1:  # extend the goal_poses to N+1
+        #     self.local_ref_path += [self.local_ref_path[-1]] * (self.N + 1 - len(self.local_ref_path))
+
+        if self.curr_pose is not None:
+            for i in range(len(self.local_ref_path)):
+                # if self.local_ref_path[i][2] < -math.pi:
+                #     self.local_ref_path[i][2] += math.pi
+                # if self.local_ref_path[i][2] > math.pi:
+                #     self.local_ref_path[i][2] -= math.pi
+                diff = self.local_ref_path[i][2] - self.curr_pose[2]
+                while diff > math.pi:
+                    diff -= 2 * math.pi
+                    diff += 2 * math.pi
+                self.local_ref_path[i][2] = self.curr_pose[2] + diff
+
+    def compute_cmd_vel_mpc(self):
+        if self.local_ref_path is None:
+            return
+
+        mpc_ref_path = path2ndarray_se2(self.local_ref_path)
+        u = self.controller.solve(self.curr_pose, mpc_ref_path, [[self.vel_ref, 0]] * self.N)
+        x_pred = self.controller.x_opti
+        pose_array = PoseArray()
+        pose_array.header.frame_id = self.world_frame
+        for i in range(self.N + 1):
+            pose = Pose()
+            pose.position.x = x_pred[i][0]
+            pose.position.y = x_pred[i][1]
+            quat = tft.quaternion_from_euler(0, 0, x_pred[i][2])
+            pose.orientation.x = quat[0]
+            pose.orientation.y = quat[1]
+            pose.orientation.z = quat[2]
+            pose.orientation.w = quat[3]
+            pose_array.poses.append(pose)
+        self.pred_pose_pub.publish(pose_array)
+
+        cmd_vel = Twist()
+        cmd_vel.linear.x = u[0][0]
+        cmd_vel.angular.z = u[0][1]
+        return cmd_vel
+
+    def compute_lqr_gain(self, A, B):
+        P = np.matrix(scipy.linalg.solve_continuous_are(A, B, self.Q, self.R))
+        K = np.matrix(scipy.linalg.inv(self.R) * (B.T * P))
+        return K
+
+    def compute_cmd_vel_lqr(self):
+        if self.local_ref_path is None or len(self.local_ref_path) == 0:
+            return Twist()
+
+        ref_pose = self.local_ref_path[0]
+        x_ref, y_ref, yaw_ref = ref_pose[0], ref_pose[1], ref_pose[2]
+        x, y, yaw = self.curr_pose[0], self.curr_pose[1], self.curr_pose[2]
+
+        x_error = x_ref - x
+        y_error = y_ref - y
+        yaw_error = yaw_ref - yaw
+
+        A = np.matrix([[0, 0, -self.vel_ref * math.sin(yaw)], [0, 0, self.vel_ref * math.cos(yaw)], [0, 0, 0]])
+        B = np.matrix([[math.cos(yaw), 0], [math.sin(yaw), 0], [0, 1]])
+
+        K = self.compute_lqr_gain(A, B)
+
+        x = np.matrix([x_error, y_error, yaw_error]).T
+        u = -K * x
+
+        cmd_vel = Twist()
+        cmd_vel.linear.x = self.vel_ref + u[0, 0]
+        cmd_vel.angular.z = u[1, 0]
+
+        return cmd_vel
+
+    def run(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            if self.local_ref_path is not None and self.curr_pose is not None and self.enable_plan:
+                if self.method == "mpc":
+                    cmd_vel = self.compute_cmd_vel_mpc()
+                elif self.method == "lqr":
+                    cmd_vel = self.compute_cmd_vel_lqr()
+                if euclidian_dist_se2(ndarray2pose_se2(self.curr_pose), self.goal_pose.pose) < self.arrive_th:
+                    self.enable_plan = False
+                    cmd_vel = Twist()
+                    rospy.loginfo("Goal reached!")
+                self.pub_cmd_vel.publish(cmd_vel)
+                self.local_ref_path_pub.publish(self.local_ref_path)
+            rate.sleep()
+
+
+if __name__ == "__main__":
+    try:
+        path_tracker_node = NMPCNode(method="mpc")
+        path_tracker_node.run()
+    except rospy.ROSInterruptException:
+        pass
---
+++ final_nav/src/utils.py
@@ -0,0 +1,161 @@
+"""
+ # @ Author: Kuankuan Sima
+ # @ Email: smkk00715@gmail.com
+ # @ Create Time: 2024-04-06 18:42:59
+ # @ Modified time: 2024-04-06 19:51:57
+ # @ Description:
+ """
+
+import geometry_msgs.msg as geometry_msgs
+import nav_msgs.msg as nav_msgs
+import tf
+import tf.transformations as tft
+import numpy as np
+
+
+def euclidian_dist_se2(p1: geometry_msgs.Pose, p2: geometry_msgs.Pose) -> float:
+    return ((p1.position.x - p2.position.x) ** 2 + (p1.position.y - p2.position.y) ** 2) ** 0.5
+
+
+def quat2yaw(q: geometry_msgs.Quaternion) -> float:
+    return tft.euler_from_quaternion([q.x, q.y, q.z, q.w])[2]
+
+
+def linear_interpolate_pose(pose1, pose2, t):
+    """
+    Linearly interpolates between two poses.
+
+    Parameters:
+    - pose1, pose2: geometry_msgs.msg.Pose, the start and end poses.
+    - t: float, interpolation factor (0 <= t <= 1).
+
+    Returns:
+    - interpolated_pose: geometry_msgs.msg.Pose, the interpolated pose.
+    """
+    interpolated_pose = geometry_msgs.PoseStamped()
+    interpolated_pose.pose.position.x = (1 - t) * pose1.pose.position.x + t * pose2.pose.position.x
+    interpolated_pose.pose.position.y = (1 - t) * pose1.pose.position.y + t * pose2.pose.position.y
+    interpolated_pose.pose.position.z = (1 - t) * pose1.pose.position.z + t * pose2.pose.position.z
+    # Orientation is not interpolated in this example for simplicity.
+    return interpolated_pose
+
+
+def extract_interpolated_path(path: nav_msgs.Path, T, N, V):
+    """
+    Extracts and interpolates a new path based on time steps and reference speed from a nav_msgs.msg.Path.
+
+    Parameters:
+    - path: nav_msgs.msg.Path, the original path.
+    - T: float, the forward recursive time step.
+    - N: int, the number of points in the new path.
+    - V: float, the reference speed.
+
+    Returns:
+    - new_path: nav_msgs.msg.Path, the new interpolated path.
+    """
+    if len(path.poses) < 1:
+        return None
+    new_path = nav_msgs.Path()
+    new_path.header = path.header  # Copy the header from the original path
+    total_distance = 0
+    distances = []
+
+    # Calculate distances between consecutive points and total distance.
+    poses = path.poses  # List of PoseStamped
+    for i in range(len(poses) - 1):
+        dx = poses[i + 1].pose.position.x - poses[i].pose.position.x
+        dy = poses[i + 1].pose.position.y - poses[i].pose.position.y
+        dz = poses[i + 1].pose.position.z - poses[i].pose.position.z
+        dist = np.sqrt(dx**2 + dy**2 + dz**2)
+        distances.append(dist)
+        total_distance += dist
+
+    # Calculate step distance based on T, V, and total distance.
+    step_distance = V * T
+    target_distance = step_distance
+
+    # Perform linear interpolation to generate new path.
+    for _ in range(N):
+        while distances and target_distance > distances[0]:
+            target_distance -= distances.pop(0)
+            poses.pop(0)
+
+        if distances:
+            t = target_distance / distances[0]
+            new_pose = linear_interpolate_pose(poses[0], poses[1], t)
+            new_path.poses.append(new_pose)
+            target_distance += step_distance
+        else:
+            break
+
+    # Ensure the new path has N points by copying the last point if necessary.
+    if len(new_path.poses) > 0:
+        while len(new_path.poses) < N:
+            new_path.poses.append(new_path.poses[-1])  # Copy the last point if new path is shorter than expected.
+    else:
+        while len(new_path.poses) < N:
+            new_path.poses.append(path.poses[0])
+    return new_path
+
+
+def ndarray2path(ar: np.ndarray) -> nav_msgs.Path:
+    path = nav_msgs.Path()
+    for i in range(ar.shape[0]):
+        pose = geometry_msgs.PoseStamped()
+        pose.pose.position.x = ar[i, 0]
+        pose.pose.position.y = ar[i, 1]
+        pose.pose.position.z = ar[i, 2]
+        pose.pose.orientation.x = ar[i, 3]
+        pose.pose.orientation.y = ar[i, 4]
+        pose.pose.orientation.z = ar[i, 5]
+        pose.pose.orientation.w = ar[i, 6]
+        path.poses.append(pose)
+    return path
+
+
+def path2ndarray_se2(path: nav_msgs.Path) -> np.ndarray:
+    """Path to ndarray with SE(2) representation."""
+    ar = np.zeros((len(path.poses), 3))
+    for i in range(len(path.poses)):
+        ar[i, 0] = path.poses[i].pose.position.x
+        ar[i, 1] = path.poses[i].pose.position.y
+        euler = tft.euler_from_quaternion(
+            [
+                path.poses[i].pose.orientation.x,
+                path.poses[i].pose.orientation.y,
+                path.poses[i].pose.orientation.z,
+                path.poses[i].pose.orientation.w,
+            ]
+        )
+        ar[i, 2] = euler[2]
+    return ar
+
+
+def ndarray2pose_se2(ar: np.ndarray) -> geometry_msgs.Pose:
+    pose = geometry_msgs.Pose()
+    pose.position.x = ar[0]
+    pose.position.y = ar[1]
+    pose.position.z = 0
+    q = tft.quaternion_from_euler(0, 0, ar[2])
+    pose.orientation.x = q[0]
+    pose.orientation.y = q[1]
+    pose.orientation.z = q[2]
+    pose.orientation.w = q[3]
+    return pose
+
+
+if __name__ == "__main__":
+
+    path = [
+        [0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1],
+        [2, 0, 0, 0, 0, 0, 1],
+        [3, 0, 0, 0, 0, 0, 1],
+    ]
+    path = ndarray2path(np.array(path))
+
+    T = 0.5
+    N = 10
+    new_path = extract_interpolated_path(path, T, N, 0.5)
+    for pose in new_path.poses:
+        print(f"({pose.pose.position.x}, {pose.pose.position.y}, {pose.pose.position.z})")
