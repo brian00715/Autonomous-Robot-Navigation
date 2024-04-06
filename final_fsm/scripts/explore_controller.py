cda726a7e6b04e8c919de86974136b63d9d10569
LuyiLi <luyili08@163.com>
2024-04-06 22:29:19 +0800

    tuned cartographer

--- final_fsm/scripts/explore_controller.py
+++ final_fsm/scripts/explore_controller.py
@@ -6,11 +6,33 @@ from std_msgs.msg import Bool
 
 pause = True
 
-def map_callback(msg):
+# ***************
+#   OBSTACLE
+M = 70
+#   unknown
+N = 50
+#   free
+# ----0-----
+#   unknown
+# ***************
+
+
+def map_callback(cmap: OccupancyGrid):
     if pause:
         return
     # 该回调函数将接收到的map消息重新发布到map_explore话题
-    pub_map.publish(msg)
+    data = list(cmap.data)
+    for y in range(cmap.info.height):
+        for x in range(cmap.info.width):
+            i = x + (cmap.info.height - 1 - y) * cmap.info.width
+            if data[i] >= M:  
+                data[i] = 100
+            elif (data[i] >= 0) and (data[i] < N):  # free
+                data[i] = 0
+            else:  # unknown
+                data[i] = -1
+    cmap.data = tuple(data)
+    pub_map.publish(cmap)
 
 def pause_callback(msg):
     global pause
@@ -28,7 +50,6 @@ if __name__ == '__main__':
 
         # 创建一个发布者，用于发布重新封装的map消息到map_explore话题
         pub_map = rospy.Publisher('/map_explore', OccupancyGrid, queue_size=10)
-
 
         rospy.spin()
     except rospy.ROSInterruptException:
