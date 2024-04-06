34e3eb935dfa6db1a89d3f39b5b72b63a255a958
zyaqcl <law7359@outlook.com>
2024-04-06 23:49:33 +0800

    basic ray

--- final_percep/visual.py
+++ final_percep/visual.py
@@ -7,6 +7,7 @@ import rospy
 from sensor_msgs.msg import Image, CameraInfo
 import easyocr
 from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import OccupancyGrid
 from std_msgs.msg import String
 from cv_bridge import CvBridge
 import cv2
@@ -29,6 +30,8 @@ class Visual:
         self.reader = easyocr.Reader(['en'])
         self.listener = tf.TransformListener()
         self.sub = rospy.Subscriber('/front/image_raw', Image, self.callback)
+        self.mapsub = rospy.Subscriber('/map', OccupancyGrid, self.map_callback)
+        self.goalsub = rospy.Subscriber('/goal', String, self.goal_callback)
         self.is_processing = False
         rospy.spin()
 
@@ -40,6 +43,12 @@ class Visual:
         gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
         result = self.reader.readtext(gray)
         for detection in result:
+            if len(detection[1]) > 1:
+                continue
+            if detection[2] < 0.9:
+                continue
+            if detection[1] < '1' or detection[1] > '9':
+                continue
             # Get the conter of the bounding box in [x, y] format
             center = [(x + y)/2 for x, y in zip(detection[0][0], detection[0][2])]
             # Get the direction of the point in camera frame, [u, v, 1]
@@ -63,7 +72,33 @@ class Visual:
             # Pulish the direction and the id of the object
             self.pubpose.publish(transformed)
             self.pubid.publish(detection[1])
+
+            # Get the position and orientation of the camera wrt the map from the tf
+            campos = np.array(self.listener.lookupTransform('map', 'front_camera_mount', rospy.Time(0))[0])
+            camori = np.array(self.listener.lookupTransform('map', 'front_camera_mount', rospy.Time(0))[1])
+
+            # Get the euler angles from the quaternion
+            euler = euler_from_quaternion(camori)
+            yaw = euler[2]
+
+            
         self.is_processing = False
+
+    def map_callback(self, msg):
+        data = list(msg.data)
+        for y in range(msg.info.height):
+            for x in range(msg.info.width):
+                i = x + (msg.info.height - 1 - y) * msg.info.width
+                if data[i] >= 75:  
+                    data[i] = 100
+                elif (data[i] >= 0) and (data[i] < 50):  # free
+                    data[i] = 0
+                else:  # unknown
+                    data[i] = -1
+        self.map = np.array(data).reshape(msg.info.height, msg.info.width)
+
+    def goal_callback(self, msg):
+        self.goal = msg.data
 
 
 
